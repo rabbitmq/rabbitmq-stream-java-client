ada5564fda65729bbbaa923ebe8a974fe63b3b2d
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-05-11 09:46:49 +0200

    Fix documentation publishing script with shallow Git clones

--- publish-documentation-to-github-pages.sh
+++ publish-documentation-to-github-pages.sh
@@ -2,6 +2,10 @@
 
 MESSAGE=$(git log -1 --pretty=%B)
 ./mvnw asciidoctor:process-asciidoc
+
+# Concourse does shallow clones, so need the next 2 commands to have the gh-pages branch
+git remote set-branches origin 'gh-pages'
+git fetch -v
 git checkout gh-pages
 cp target/generated-docs/index.html snapshot/htmlsingle
 git add snapshot/
