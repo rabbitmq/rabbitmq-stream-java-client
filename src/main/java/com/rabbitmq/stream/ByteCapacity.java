47643005b0d1d7d861818117b5707f54b8705adb
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-05-27 11:00:57 +0200

    Add parse function in ByteCapacity

--- src/main/java/com/rabbitmq/stream/ByteCapacity.java
+++ src/main/java/com/rabbitmq/stream/ByteCapacity.java
@@ -14,12 +14,35 @@
 
 package com.rabbitmq.stream;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ByteCapacity {
 
+    private static final String UNIT_MB = "mb";
     private static final int KILOBYTES_MULTIPLIER = 1000;
     private static final int MEGABYTES_MULTIPLIER = 1000 * 1000;
     private static final int GIGABYTES_MULTIPLIER = 1000 * 1000 * 1000;
-    private static final int TERABYTES_MULTIPLIER = 1000 * 1000 * 1000;
+    private static final long TERABYTES_MULTIPLIER = 1000L * 1000L * 1000L * 1000L;
+
+    private static final Pattern PATTERN = Pattern.compile("^(?<size>\\d*)((?<unit>kb|mb|gb|tb))?$", Pattern.CASE_INSENSITIVE);
+    private static final String GROUP_SIZE = "size";
+    private static final String GROUP_UNIT = "unit";
+    private static final String UNIT_KB = "kb";
+    private static final String UNIT_GB = "gb";
+    private static final String UNIT_TB = "tb";
+
+    private static final Map<String, Function<Long, ByteCapacity>> CONSTRUCTORS = Collections.unmodifiableMap(new HashMap<String, Function<Long, ByteCapacity>>() {{
+        put(UNIT_KB, size -> ByteCapacity.kB(size));
+        put(UNIT_MB, size -> ByteCapacity.MB(size));
+        put(UNIT_GB, size -> ByteCapacity.GB(size));
+        put(UNIT_TB, size -> ByteCapacity.TB(size));
+    }});
+
 
     private final long bytes;
 
@@ -49,6 +72,26 @@ public class ByteCapacity {
 
     public long toBytes() {
         return bytes;
+    }
+
+    public static ByteCapacity from(String value) {
+        Matcher matcher = PATTERN.matcher(value);
+        if (matcher.matches()) {
+            long size = Long.valueOf(matcher.group(GROUP_SIZE));
+            String unit = matcher.group(GROUP_UNIT);
+            ByteCapacity result;
+            if (unit == null) {
+                result = ByteCapacity.B(size);
+            } else {
+                return CONSTRUCTORS.getOrDefault(unit.toLowerCase(),
+                        s -> {
+                            throw new IllegalArgumentException("Unknown capacity unit: " + unit);
+                        }).apply(size);
+            }
+            return result;
+        } else {
+            throw new IllegalArgumentException("Cannot parse value for byte capacity: " + value);
+        }
     }
 
 }
---
+++ src/test/java/com/rabbitmq/stream/ByteCapacityTest.java
@@ -0,0 +1,59 @@
+// Copyright (c) 2020 VMware, Inc. or its affiliates.  All rights reserved.
+//
+// This software, the RabbitMQ Java client library, is dual-licensed under the
+// Mozilla Public License 1.1 ("MPL"), and the Apache License version 2 ("ASL").
+// For the MPL, please see LICENSE-MPL-RabbitMQ. For the ASL,
+// please see LICENSE-APACHE2.
+//
+// This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+// either express or implied. See the LICENSE file for specific language governing
+// rights and limitations of this software.
+//
+// If you have any questions regarding licensing, please contact us at
+// info@rabbitmq.com.
+
+package com.rabbitmq.stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.of;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class ByteCapacityTest {
+
+    static Stream<Arguments> fromOkArguments() {
+        return of(
+                arguments("100tb", 100_000_000_000_000L),
+                arguments("100gb", 100_000_000_000L),
+                arguments("100mb", 100_000_000L),
+                arguments("100kb", 100_000L),
+                arguments("100", 100L)
+        ).flatMap(arguments -> Stream.of(arguments, arguments(
+                arguments.get()[0].toString().toUpperCase(),
+                arguments.get()[1]
+        )));
+    }
+
+    @ParameterizedTest
+    @MethodSource("fromOkArguments")
+    void fromOk(String value, long expectedValueInBytes) {
+        ByteCapacity capacity = ByteCapacity.from(value);
+        assertThat(capacity.toBytes()).isEqualTo(expectedValueInBytes);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "100.0gb", "abc", "100.0", "-10gb", "10b"
+    })
+    void fromKo(String value) {
+        assertThatThrownBy(() -> ByteCapacity.from(value)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
