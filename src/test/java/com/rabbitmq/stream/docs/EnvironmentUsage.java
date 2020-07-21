bc5e5679986380b605dc2f2a4978d52fcf425e9b
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-07-21 10:41:16 +0200

    Document Environment API

---
+++ src/test/java/com/rabbitmq/stream/docs/EnvironmentUsage.java
@@ -0,0 +1,75 @@
+// Copyright (c) 2020 VMware, Inc. or its affiliates.  All rights reserved.
+//
+// This software, the RabbitMQ Stream Java client library, is dual-licensed under the
+// Mozilla Public License 2.0 ("MPL"), and the Apache License version 2 ("ASL").
+// For the MPL, please see LICENSE-MPL-RabbitMQ. For the ASL,
+// please see LICENSE-APACHE2.
+//
+// This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+// either express or implied. See the LICENSE file for specific language governing
+// rights and limitations of this software.
+//
+// If you have any questions regarding licensing, please contact us at
+// info@rabbitmq.com.
+
+package com.rabbitmq.stream.docs;
+
+import com.rabbitmq.stream.ByteCapacity;
+import com.rabbitmq.stream.Environment;
+
+import java.util.Arrays;
+
+public class EnvironmentUsage {
+
+    void environmentCreation() {
+        // tag::environment-creation[]
+        Environment environment = Environment.builder().build(); // <1>
+        // end::environment-creation[]
+    }
+
+    void environmentCreationWithUri() {
+        // tag::environment-creation-with-uri[]
+        Environment environment = Environment.builder()
+                .uri("rabbitmq-stream://guest:guest@localhost:5555/%2f") // <1>
+                .build();
+        // end::environment-creation-with-uri[]
+    }
+
+    void environmentCreationWithUris() {
+        // tag::environment-creation-with-uris[]
+        Environment environment = Environment.builder()
+                .uris(Arrays.asList(                     // <1>
+                        "rabbitmq-stream://host1:5555",
+                        "rabbitmq-stream://host2:5555",
+                        "rabbitmq-stream://host3:5555")
+                )
+                .build();
+        // end::environment-creation-with-uris[]
+    }
+
+    void createStream() {
+        Environment environment = Environment.builder().build();
+        // tag::stream-creation[]
+        environment.streamCreator().stream("my-stream").create(); // <1>
+        // end::stream-creation[]
+    }
+
+    void createStreamWithRetention() {
+        Environment environment = Environment.builder().build();
+        // tag::stream-creation-retention[]
+        environment.streamCreator()
+                .stream("my-stream")
+                .maxLengthBytes(ByteCapacity.GB(10))       // <1>
+                .maxSegmentSizeBytes(ByteCapacity.MB(500)) // <2>
+                .create();
+        // end::stream-creation-retention[]
+    }
+
+    void deleteStream() {
+        Environment environment = Environment.builder().build();
+        // tag::stream-deletion[]
+        environment.deleteStream("my-stream"); // <1>
+        // end::stream-deletion[]
+    }
+
+}
