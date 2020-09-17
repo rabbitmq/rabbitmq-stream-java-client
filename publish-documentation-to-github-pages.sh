72f741abf2ba08c7ca5ece38ac735144e97faffe
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-09-17 15:54:00 +0200

    Add PDF rendering to documentation

--- publish-documentation-to-github-pages.sh
+++ publish-documentation-to-github-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MESSAGE=$(git log -1 --pretty=%B)
-./mvnw asciidoctor:process-asciidoc
+./mvnw clean buildnumber:create pre-site
 
 # Concourse does shallow clones, so need the next 2 commands to have the gh-pages branch
 git remote set-branches origin 'gh-pages'
@@ -9,6 +9,8 @@ git fetch -v
 git checkout gh-pages
 mkdir -p snapshot/htmlsingle
 cp target/generated-docs/index.html snapshot/htmlsingle
+mkdir -p snapshot/pdf
+cp target/generated-docs/index.pdf snapshot/pdf
 git add snapshot/
 git commit -m "$MESSAGE"
 git push origin gh-pages
