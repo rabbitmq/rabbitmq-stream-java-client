d3aed380fff67581408e04840bad771f4596e7c1
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2022-12-05 10:13:16 +0100

    Activate TLS in GHA

--- ci/start-broker.sh
+++ ci/start-broker.sh
@@ -13,17 +13,35 @@ wait_for_message() {
   done
 }
 
-cat > ${PWD}/enabled_plugins << ENDOFFILE
-[rabbitmq_stream,rabbitmq_mqtt,rabbitmq_stomp].
-ENDOFFILE
+make -C "${PWD}"/tls-gen/basic
+
+mkdir rabbitmq-configuration
+
+echo "[rabbitmq_stream,rabbitmq_mqtt,rabbitmq_stomp]." >> rabbitmq-configuration/enabled_plugins
+
+echo "loopback_users = none
+
+listeners.ssl.default = 5671
+
+ssl_options.cacertfile = ${PWD}/tls-gen/basic/result/ca_certificate.pem
+ssl_options.certfile   = ${PWD}/tls-gen/basic/result/server_$(hostname)_certificate.pem
+ssl_options.keyfile    = ${PWD}/tls-gen/basic/result/server_$(hostname)_key.pem
+ssl_options.verify     = verify_peer
+ssl_options.fail_if_no_peer_cert = false
+
+stream.listeners.ssl.1 = 5551" >> rabbitmq-configuration/rabbitmq.conf
+
 
 echo "Running RabbitMQ ${RABBITMQ_IMAGE}:${RABBITMQ_IMAGE_TAG}"
 
 docker rm -f rabbitmq 2>/dev/null || echo "rabbitmq was not running"
 docker run -d --name rabbitmq \
     -p 5672:5672 -p 5552:5552 -p 1883:1883 -p 61613:61613 \
-    -v "${PWD}"/enabled_plugins:/etc/rabbitmq/enabled_plugins \
+    -v "${PWD}"/rabbitmq-configuration:/etc/rabbitmq \
     -e RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS='-rabbitmq_stream advertised_host localhost' \
     "${RABBITMQ_IMAGE}":"${RABBITMQ_IMAGE_TAG}"
 
 wait_for_message rabbitmq "Server startup complete"
+
+docker exec rabbitmq rabbitmq-diagnostics erlang_version
+docker exec rabbitmq rabbitmqctl version
