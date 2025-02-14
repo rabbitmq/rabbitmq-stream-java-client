9e1a372baf22ce7e8abd9a4be5294edec9371c2e
Arnaud Cogoluègnes <acogoluegnes@tuta.io>
2025-02-14 11:32:12 +0100

    Bump Netty to 4.2.0.RC3

--- src/main/java/com/rabbitmq/stream/impl/Client.java
+++ src/main/java/com/rabbitmq/stream/impl/Client.java
@@ -59,17 +59,7 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.ByteBufOutputStream;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
-import io.netty.channel.ConnectTimeoutException;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DecoderException;
@@ -248,7 +238,7 @@ public class Client implements AutoCloseable {
     if (b.config().group() == null) {
       EventLoopGroup eventLoopGroup;
       if (parameters.eventLoopGroup == null) {
-        this.eventLoopGroup = new NioEventLoopGroup();
+        this.eventLoopGroup = Utils.eventLoopGroup();
         eventLoopGroup = this.eventLoopGroup;
       } else {
         this.eventLoopGroup = null;
